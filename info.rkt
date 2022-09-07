23360a6e33823a7d123f1fba6017f4aa4520f8bc
Sam Phillips <samdphillips@gmail.com>
2022-09-07 15:55:19 -0700

    Add build-dep for racket-doc

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define collection "inifile")
 (define version "0.2")
 (define deps '("base"))
-(define build-deps '("rackunit-lib" "scribble-lib"))
+(define build-deps '("racket-doc" "rackunit-lib" "scribble-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define scribblings '(["scribblings/inifile.scrbl" ()]))
